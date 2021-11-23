d39baebc9fd1bbf37211404867979a492b82d3a7
Chris Wright <37849366+chriswr95@users.noreply.github.com>
2021-11-23 04:47:58 +0000

    setup pomodoro timer

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace pomodoro_cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ValidateArgs(args);
+            var topic = ParseArgs(args);
+            var timer = new System.Timers.Timer();
+            var pomodoro =  new Pomodoro(timer, topic);
+            pomodoro.Run();
+        }
+
+        private static void ValidateArgs(string[] args)
+        {
+            if (args.Length != 1) {
+                throw new Exception("Error: Include only one argument!");
+            }
+        }
+
+        private static String ParseArgs(string[] args)
+        {
+            return Convert.ToString(args[0]);
+        }
+    }
+}
---
+++ pomodoro/Pomodoro.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Timers;
+namespace pomodoro_cli
+{
+    class Pomodoro{
+
+        private Timer timer;
+        private string topic;
+
+        private long ticksRemaining = (25 * TimeSpan.TicksPerMinute) - TimeSpan.TicksPerSecond;
+
+        public Pomodoro(Timer timer, string topic)
+        {
+            this.timer = timer;
+            this.topic = topic;
+        }
+
+        public void Run()
+        {
+            Console.WriteLine($"Beginning Pomodoro for {topic}");
+            Start();
+            Console.ReadKey();
+            Stop();
+        }
+
+        private void Start()
+        {
+            timer.Elapsed += OnTick;
+            timer.Interval = 1000;
+            timer.AutoReset = true;
+            timer.Enabled = true;
+            
+        }
+
+        private void Stop()
+        {
+            timer.Stop();
+            timer.Dispose();
+        }
+
+        private void OnTick(Object source, ElapsedEventArgs e)
+        {
+            var timeSpan = new TimeSpan(ticksRemaining);
+            Console.Write($"\r{timeSpan.Minutes}:{timeSpan.Seconds}");
+            ticksRemaining = ticksRemaining - TimeSpan.TicksPerSecond;
+        }
+    }
+}
